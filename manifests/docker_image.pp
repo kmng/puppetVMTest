a757c08ca4ed20b34cc3d96c0759a63d940c45e7
kmng <kmng@ucla.edu>
2019-04-30 15:40:10 -0700

    try docker volume

--- manifests/docker_image.pp
+++ manifests/docker_image.pp
@@ -7,3 +7,9 @@ docker::run { 'hello':
   image   => 'bitfield/hello',
   command => '/bin/sh -c "while true; do echo Hello, world; sleep 1; done"',
 }
+
+docker::run { 'mount_test':
+  image   => 'library/alpine:3.6',
+  volumes => ['/tmp/container_data:/mnt/data'],
+  command => '/bin/sh -c "echo Hello, world >/mnt/data/hello.txt"',
+}
